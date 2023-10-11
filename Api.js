3a95608358981a77e779f989ff4c45b329bacd31
Jhon <jhonlaurens@gmail.com>
2023-10-10 23:14:28 -0500

    Design refinements a
    Design refinements and improvements have been made.

--- Api.js
+++ Api.js
@@ -4,8 +4,8 @@ let total = 0;
 
 // Función para agregar un producto al carrito
 function addToCart(productName, productPrice) {
-    cart.push({ name: productName, price: productPrice * 1000 });
-    total += productPrice * 1000;
+    cart.push({ name: productName, price: productPrice });
+    total += productPrice;
     updateCartUI();
 }
 
@@ -24,6 +24,13 @@ function updateCartUI() {
     cartTotalElement.textContent = `$${total}`;
 }
 
+// Función para limpiar el carrito
+function clearCart() {
+    cart.length = 0;
+    total = 0;
+    updateCartUI();
+}
+
 // Manejo de clic en los botones de compra
 const buyButtons = document.querySelectorAll(".btn-2");
 buyButtons.forEach(button => {
@@ -31,13 +38,25 @@ buyButtons.forEach(button => {
         const productElement = event.target.closest(".product-1");
         const productName = productElement.querySelector("h3").textContent;
         const productPrice = parseFloat(productElement.querySelector(".price p").textContent.replace("COP $", ""));
-        addToCart(productName, productPrice);
+        const confirmPurchase = confirm(`¿Estás seguro de que deseas comprar ${productName} por COP $${productPrice}?`);
+        if (confirmPurchase) {
+            addToCart(productName, productPrice);
+        }
     });
 });
 
-
 const checkoutBtn = document.getElementById("checkout-btn");
 checkoutBtn.addEventListener("click", () => {
+    const confirmCheckout = confirm(`Total a pagar: COP $${total}. ¿Estás seguro de continuar con el pago?`);
+    if (confirmCheckout) {
+        alert("Redireccionando a la pasarela de pago...");
+    }
+});
 
-    alert(`Total a pagar: COP $${total}. Redireccionando a la pasarela de pago...`);
+const clearCartBtn = document.getElementById("clear-cart-btn");
+clearCartBtn.addEventListener("click", () => {
+    const confirmClear = confirm("¿Estás seguro de que deseas limpiar el carrito?");
+    if (confirmClear) {
+        clearCart();
+    }
 });
